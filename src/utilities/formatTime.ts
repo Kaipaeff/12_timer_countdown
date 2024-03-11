db8e8204e442c237026f464e4bdecafdb519add8
Aslanbek <kaipaeff@gmail.com>
2024-03-11 15:14:33 +0300

    Рефактор CircularProgress и formatTime

--- src/utilities/formatTime.ts
+++ src/utilities/formatTime.ts
@@ -2,11 +2,20 @@ export const formatTimerTime = (totalSeconds: number): string => {
   const minutes = Math.floor(totalSeconds / 60_000);
   const seconds = Math.floor((totalSeconds % 60_000) / 1000);
   const milliseconds = Math.floor((totalSeconds % 1000) / 10);
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(2, '0')}`;
+
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  const formattedMilliseconds = milliseconds < 10 ? `0${milliseconds}` : milliseconds;
+
+  return `${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}`;
 }
 
 export const formatCountDownTime = (totalSeconds: number): string => {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = Math.floor(totalSeconds % 60);
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
