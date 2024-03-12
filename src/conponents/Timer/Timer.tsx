9ca1100ca30818c31bb4e05e4414096fa68760ae
Aslanbek <kaipaeff@gmail.com>
2024-03-12 08:07:50 +0300

    Prettier & Eslint configured

--- src/conponents/Timer/Timer.tsx
+++ src/conponents/Timer/Timer.tsx
@@ -1,23 +1,21 @@
-import { useCallback, memo } from "react";
+import { useCallback, memo } from 'react';
 
-import CircularProgress from "../CircularProgress/CircularProgress";
+import CircularProgress from '../CircularProgress/CircularProgress';
 
-import { InnerWrapperBlockStyles } from "../../styles/InnerWrapperBlockStyles";
-import { CancelControlButtonStyles, StartControlButtonStyles, StopControlButtonStyles } from "../ControlButton/ControlButton.styles";
-import { ITimerComponentProps } from "../../types/interfaces";
+import { InnerWrapperBlockStyles } from '../../styles/InnerWrapperBlockStyles';
+import {
+  CancelControlButtonStyles,
+  StartControlButtonStyles,
+  StopControlButtonStyles,
+} from '../ControlButton/ControlButton.styles';
+import { ITimerComponentProps } from '../../types/interfaces';
 
-function Timer({...props}: ITimerComponentProps) {
-  const {
-    isStarted, 
-    setIsStarted, 
-    timerSeconds, 
-    setTimerSeconds, 
-    timerIntervalIdRef
-  } = props;
+function Timer({ ...props }: ITimerComponentProps) {
+  const { isStarted, setIsStarted, timerSeconds, setTimerSeconds, timerIntervalIdRef } = props;
 
   const handleStart = useCallback(() => {
-    if(!isStarted) {
-      timerIntervalIdRef!.current = setInterval(() => {  
+    if (!isStarted) {
+      timerIntervalIdRef!.current = setInterval(() => {
         setTimerSeconds((prev: number) => prev + 10);
       }, 10);
     } else {
@@ -31,22 +29,29 @@ function Timer({...props}: ITimerComponentProps) {
     setIsStarted(false);
     clearInterval(timerIntervalIdRef!.current);
     timerIntervalIdRef!.current = undefined;
-  }, [timerIntervalIdRef!.current])
+  }, [timerIntervalIdRef!.current]);
 
   return (
     <>
-      <CircularProgress 
-        timerSeconds={timerSeconds} 
-        timerIntervalIdRef={timerIntervalIdRef} 
-        isStarted={isStarted}
-      />
+      <CircularProgress timerSeconds={timerSeconds} timerIntervalIdRef={timerIntervalIdRef} isStarted={isStarted} />
       <InnerWrapperBlockStyles>
-        <CancelControlButtonStyles onClick={handleReset} title={'Сброс'} disabled={!timerSeconds || isStarted} backgroundColor1="white"/>
-        {!isStarted && <StartControlButtonStyles onClick={handleStart} title={!timerSeconds ? 'Старт' : 'Дальше'} backgroundColor1="white"/>}
-        {isStarted && <StopControlButtonStyles onClick={handleStart} title={'Стоп'} backgroundColor1="white"/>}
+        <CancelControlButtonStyles
+          onClick={handleReset}
+          title={'Сброс'}
+          disabled={!timerSeconds || isStarted}
+          backgroundColor1="white"
+        />
+        {!isStarted && (
+          <StartControlButtonStyles
+            onClick={handleStart}
+            title={!timerSeconds ? 'Старт' : 'Дальше'}
+            backgroundColor1="white"
+          />
+        )}
+        {isStarted && <StopControlButtonStyles onClick={handleStart} title={'Стоп'} backgroundColor1="white" />}
       </InnerWrapperBlockStyles>
     </>
-  )
+  );
 }
 
 export default memo(Timer);
